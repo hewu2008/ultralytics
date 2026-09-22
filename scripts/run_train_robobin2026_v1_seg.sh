366f96627fa22b8c3896702c54d7dc094ec924aa
hewu2008 <hewu2008@gmail.com>
2026-09-22 19:04:44 +0800

    build: 增大训练批次和启用多GPU设备
    调整训练配置，将批次大小从32提升至64，并启用两块GPU进行训练以提升训练效率

--- scripts/run_train_robobin2026_v1_seg.sh
+++ scripts/run_train_robobin2026_v1_seg.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 set -e
 
-export CUDA_VISIBLE_DEVICES=0
+export CUDA_VISIBLE_DEVICES=0,1
 
 yolo train \
   cfg=assets/robobin2026-v1-seg-finetune.yaml \
   model=assets/yolo26l-seg.pt \
-  data=assets/robobin2026-v1-seg.yaml
+  data=assets/robobin2026-v1-seg.yaml \
+  device=0,1
