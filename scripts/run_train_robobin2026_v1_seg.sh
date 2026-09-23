e632452f2601fa4c152ee8e7877f54650d12bc59
hewu2008 <hewu2008@gmail.com>
2026-09-23 13:19:39 +0800

    build: 调整RoboBin2026分割训练脚本与配置参数

--- scripts/run_train_robobin2026_v1_seg.sh
+++ scripts/run_train_robobin2026_v1_seg.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 set -e
 
-export CUDA_VISIBLE_DEVICES=0,1
+export CUDA_VISIBLE_DEVICES=0
 
 yolo train \
   cfg=assets/robobin2026-v1-seg-finetune.yaml \
   model=assets/yolo26l-seg.pt \
   data=assets/robobin2026-v1-seg.yaml \
-  device=0,1
+  cls_pw=0.5 \
+  device=0
